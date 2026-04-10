084a10a88128c19933e969eeb6d521c97f1688a4
taneesha-kohli <shivanirana1245@gmail.com>
2026-04-10 17:56:15 +0530

    Did some change in server file

--- server/server.js
+++ server/server.js
@@ -15,7 +15,10 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-  origin: 'https://mern-eco-friendly-ecommerce.vercel.app',
+  origin: [
+      'http://localhost:5173',
+       'https://mern-eco-friendly-ecommerce.vercel.app'
+  ],
   credentials: true
 }))
 
