5c1785f3d3b97f3da0b8023adbef8e7ca88362c2
taneesha-kohli <shivanirana1245@gmail.com>
2026-04-10 18:05:53 +0530

    modified server.js

--- server/server.js
+++ server/server.js
@@ -15,10 +15,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-  origin: [
-      'http://localhost:5173',
-       'https://mern-eco-friendly-ecommerce.vercel.app'
-  ],
+  origin: 'https://mern-eco-friendly-ecommerce.vercel.app',
   credentials: true
 }))
 
